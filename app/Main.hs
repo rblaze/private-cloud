2614fc96788efe3bb674a9fa6b8be56b117b2575
Andrey Sverdlichenko <blaze@ruddy.ru>
2017-01-24 18:18:36 -0800

    Make sample 2-way sync app

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Control.Concurrent
 import Control.Monad
+import System.Directory
 import System.FilePath
 import System.Log.Logger
+import System.Posix.Files
 import qualified Data.ByteString.Lazy as BL
 
 import PrivateCloud.Aws
@@ -31,12 +33,12 @@ main = do
 
     withDatabase (rootDir </> dbName) $ \conn -> do
         noticeM mainLoggerName "#DBOPEN"
-        let loop = do
+        forever $ do
                 localFiles <- unrollTreeFiles <$> makeTree rootDir
                 dbFiles <- getFileList conn
-                diff <- getLocalChanges rootDir localFiles dbFiles
+                localDiff <- getLocalChanges rootDir localFiles dbFiles
 
-                forM_ diff $ \(f, i) -> case i of
+                forM_ localDiff $ \(f, i) -> case i of
                     ContentChange info -> do
                         body <- BL.readFile (rootDir </> f)
                         uploadFile config f body
@@ -50,6 +52,21 @@ main = do
                         removeFileInfo config f
                         deleteFileInfo conn f
 
+                newDbFiles <- getFileList conn
+                serverFiles <- getServerFiles config
+                serverDiff <- getServerChanges newDbFiles serverFiles
+                forM_ serverDiff $ \(f, i) -> case i of
+                    ContentChange info -> do
+                        body <- downloadFile config f
+                        createDirectoryIfMissing True (dropFileName $ rootDir </> f)
+                        BL.writeFile (rootDir </> f) body
+                        setFileTimes (rootDir </> f) (fiModTime info) (fiModTime info)
+                        putFileInfo conn f info
+                    MetadataOnlyChange info -> do
+                        setFileTimes (rootDir </> f) (fiModTime info) (fiModTime info)
+                        putFileInfo conn f info
+                    Deleted -> do
+                        removeFile (rootDir </> f)
+                        deleteFileInfo conn f
+
                 threadDelay 10000000
-                loop
-        loop
